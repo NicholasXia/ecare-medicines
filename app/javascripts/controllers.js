d0eb679fedb65070780a92f6ca43eea0ad3d1155
liruirui <HiMGloves@gmail.com>
2016-01-05 21:24:08 +0800

    local commit

--- app/javascripts/controllers.js
+++ app/javascripts/controllers.js
@@ -51,6 +51,13 @@ angular.module('medicine.controllers', [])
                 password: $scope.account.password,
                 verifycode: $scope.account.verCode
             }
+            if ($scope.account.verCode.length !== 4 || $scope.account.password.length == 0){
+                $ionicPopup.alert({
+                    title: '错误提示',
+                    template: '请输入个人正确的信息'
+                })
+                return
+            }
             createUser.save({}, user, function (data) {
                 var popup = $ionicPopup.alert({
                     title: '注册成功',
