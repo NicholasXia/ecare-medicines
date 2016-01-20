0fcb1308c7b3ab0aab0b61e9eeb1c0db6b261980
liruirui <HiMGloves@gmail.com>
2016-01-20 11:11:02 +0800

    修复指令问题

--- app/javascripts/directive.js
+++ app/javascripts/directive.js
@@ -1,7 +1,5 @@
 angular.module('medicine.directive', [])
-    .directive('image', function ($q) {
-        'use strict'
-
+    .directive('image', function($q) {
         var URL = window.URL || window.webkitURL;
 
         var getResizeArea = function () {
@@ -30,7 +28,6 @@ angular.module('medicine.directive', [])
             var height = origImage.height;
             var width = origImage.width;
 
-            // calculate the width and height, constraining the proportions
             if (width > height) {
                 if (width > maxWidth) {
                     height = Math.round(height *= maxWidth / width);
@@ -46,17 +43,15 @@ angular.module('medicine.directive', [])
             canvas.width = width;
             canvas.height = height;
 
-            //draw image on canvas
             var ctx = canvas.getContext("2d");
             ctx.drawImage(origImage, 0, 0, width, height);
 
-            // get the data from canvas as 70% jpg (or specified type).
             return canvas.toDataURL(type, quality);
         };
 
-        var createImage = function (url, callback) {
+        var createImage = function(url, callback) {
             var image = new Image();
-            image.onload = function () {
+            image.onload = function() {
                 callback(image);
             };
             image.src = url;
@@ -84,8 +79,8 @@ angular.module('medicine.directive', [])
             },
             link: function postLink(scope, element, attrs, ctrl) {
 
-                var doResizing = function (imageResult, callback) {
-                    createImage(imageResult.url, function (image) {
+                var doResizing = function(imageResult, callback) {
+                    createImage(imageResult.url, function(image) {
                         var dataURL = resizeImage(image, scope);
                         imageResult.resized = {
                             dataURL: dataURL,
@@ -95,44 +90,47 @@ angular.module('medicine.directive', [])
                     });
                 };
 
-                var applyScope = function (imageResult) {
-                    scope.$apply(function () {
+                var applyScope = function(imageResult) {
+                    scope.$apply(function() {
                         //console.log(imageResult);
-                        if (attrs.multiple)
+                        if(attrs.multiple)
                             scope.image.push(imageResult);
                         else
                             scope.image = imageResult;
                     });
                 };
 
+                var processImage =  function (file) {
+                    //create a result object for each file in files
+                    var imageResult = {
+                        file: file,
+                        url: URL.createObjectURL(file)
+                    };
+
+                    fileToDataURL(file).then(function (dataURL) {
+                        imageResult.dataURL = dataURL;
+                    });
+
+                    if(scope.resizeMaxHeight || scope.resizeMaxWidth) { //resize image
+                        doResizing(imageResult, function(imageResult) {
+                            applyScope(imageResult);
+                        });
+                    }
+                    else { //no resizing
+                        applyScope(imageResult);
+                    }
+                };
 
                 element.bind('change', function (evt) {
                     //when multiple always return an array of images
-                    if (attrs.multiple)
+                    if(attrs.multiple)
                         scope.image = [];
 
                     var files = evt.target.files;
-                    for (var i = 0; i < files.length; i++) {
-                        //create a result object for each file in files
-                        var imageResult = {
-                            file: files[i],
-                            url: URL.createObjectURL(files[i])
-                        };
-
-                        fileToDataURL(files[i]).then(function (dataURL) {
-                            imageResult.dataURL = dataURL;
-                        });
-
-                        if (scope.resizeMaxHeight || scope.resizeMaxWidth) { //resize image
-                            doResizing(imageResult, function (imageResult) {
-                                applyScope(imageResult);
-                            });
-                        }
-                        else { //no resizing
-                            applyScope(imageResult);
-                        }
+                    for(var i = 0; i < files.length; i++) {
+                        processImage(files[i]);
                     }
                 });
             }
         };
-    });
+    })
